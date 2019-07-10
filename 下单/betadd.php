c12e4b796bbc5e612df838481e62b2565471b363
a47214704 <a47214704@gmail.com>
2019-07-10 18:07:49 +0800

    UPLOAD SQL

--- 下单/betadd.php
+++ 下单/betadd.php
@@ -1,15 +1,52 @@
 <?php
-		try {
+	try {
 		$db = new PDO("mysql:host=13.209.72.196;dbname=lottery_2", "root", "123456", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		$db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch (PDOException $e) {
-			die($e->getMessage());
+	} catch (PDOException $e) {
+		die($e->getMessage());
+	}
+	$data = getdata($_POST);
+	if($data['type'] == ''){
+		echo '{"status":"fail","msg":"请选择类别","redirect":""}';
+	}else if($data['number_1'] == ''){
+		for($i=1;$i<=10;$i++){
+			if($data['number_'.$i] == ''){
+				echo '{"status":"fail","msg":"请选择号码","redirect":""}';
+			}
 		}
-		if($_POST['status'] = ''){
-			echo '{"status":"fail","msg":"资料不完整","redirect":""}';
-		}else if($_POST['number'] = ''){
-			echo '{"status":"fail","msg":"资料不完整","redirect":""}';
+	}else{
+		$number ='';
+		for($i=1;$i<=10;$i++){
+			if($i == 10){
+				$number .= $data['number_'.$i];
+			}else{
+				$number .= $data['number_'.$i].',';
+			}
 		}
-		$sql = 'Insert into lottery_bet values('.$_POST['status'].','.$_POST['status'].')';
-		echo '{"status":"ok","msg":"success","redirect":""}';
+		$sql = 'INSERT INTO `lottery_bet`(`bet_type`, `bet_number`, `bet_timestamp`, `bet_openexpect`, `bet_time`, `bet_note`) 
+			VALUES (:bet_type,:bet_number,:bet_timestamp,:bet_openexpect,:bet_time,:bet_note)';
+		$statement = $db->prepare($sql);
+		$statement->execute([
+			'bet_type' => $data['type'],
+			'bet_number' => $number,
+			'bet_timestamp' => time(),
+			'bet_openexpect' => '12345',
+			'bet_time' => date('Y-m-d H:i:s'),
+			'bet_note' => ''
+		]);
+		echo '{"status":"ok","msg":"'.$number.'","redirect":""}';
+		
+	}
+	
+	
+	function getData(){
+		foreach($_POST as $key=>$value){
+			if(is_array($value)){
+				$data[$key] = $value;
+			}else{
+				$data[$key] = trim($value); 
+			}
+		}
+		return $data;
+	}
 ?>
--- 下单/lotteryBet.php
+++ 下单/lotteryBet.php
@@ -10,19 +10,19 @@
 	</script>
 	<?php
 	define('__ROOT__',dirname(__FILE__));
-	require_once(__ROOT__.'/betadd.php');
 	try {
 		$db = new PDO("mysql:host=13.209.72.196;dbname=lottery_2", "root", "123456", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		$db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch (PDOException $e) {
 		die($e->getMessage());
 	}
-	$sql="Select * from lottery_record where type='bjpk10'";
-	$result=$db->query($sql);
-	while($row=$result->fetch(PDO::FETCH_OBJ)){    
-		echo $row->type."\n";  
-		echo $row->expect."\n";   
-	}
+	$sql="SELECT MAX(expect) as max,type FROM lottery_record group by type";
+	//$sth = $db->prepare($sql);
+	//$sth->execute();
+	$sth=$db->query($sql);
+	$row=$sth->fetchAll(PDO::FETCH_ASSOC);
+		echo $row[0]['max']."\n";   
+		echo $row[1]['max']."\n"; 
 	?>
 </head>
 
@@ -31,17 +31,18 @@
 		<h2>彩票下注</h2>
 	</div>
 	<form class="form-horizontal" id="form_add">
-		<table class="table">
 			<tr>
 				<div class="col-sm-3" style="padding-top:50px">
 					<div class="input-group-prepend">
 						<p class="text-uppercase">选择下注类型 : </p>
-						<label class="label_style"><input name="status" value="xyft" type="radio" aria-label="Radio button for following text input">飞艇</label>
-						<label class="label_style"><input name="status" value="bjpk10 " type="radio" aria-label="Radio button for following text input">PK10</label>
+						<label class="label_style"><input onclick="checkbox_xyft()" checked="checked" name="type" value="xyft" type="radio" aria-label="Radio button for following text input">飞艇</label>
+						<label class="label_style"><input onclick="checkbox_pk10()" name="type" value="bjpk10" type="radio" aria-label="Radio button for following text input">PK10</label>
 					</div>
 				</div>
-				<div class="col-sm-3">
-					<p class="text-uppercase">下旗开奖号 : </p>
+				<div id="target" class="col-sm-3">
+					<script id="template" type="text/ractive">
+						<p class="text-uppercase">下旗开奖号 : {{type}}</p>
+					</script>
 				</div>
 				<div class="col-sm-3">
 					<p class="text-uppercase">下棋开奖时间 : </p>
@@ -52,11 +53,11 @@
 					<p class="text-uppercase">下注号码 : </p>
 				</div>
 				<?php for($i=1;$i<=10;$i++){?>
-				<select name="number_<?php echo $i?>" style="margin:10px;width:150px" class="custom-select mb-3">
-					<?php for($j=1;$j<=10;$j++){?>
-						<option value="<?php echo $j?>"><?php echo $j?></option>
-					<?php }?>
-				</select>
+					<select name="number_<?php echo $i?>" style="margin:10px;width:150px" class="custom-select mb-3">
+						<?php for($j=1;$j<=10;$j++){?>
+							<option value="<?php echo $j?>"><?php echo $j?></option>
+						<?php }?>
+					</select>
 				<?php }?>
 			</tr>
 			<tr>
@@ -64,10 +65,28 @@
 					<button type="button" class="btn btn-outline-primary" onclick="pushbtn()">确认下注</button>
 				</div>
 			</tr>
-		</table>
 	</form>
 	
 	<script>
+		ractive = new Ractive({
+			target: '#target',
+			template: '#template',
+			data: {
+				item : {},
+				expect : {},
+				type: <?php echo $row[0]['max']+1;?>
+			},xyft : function(){
+				this.set("type",<?php echo $row[0]['max']+1?>);
+			},pk10 : function(){
+				this.set("type",<?php echo $row[1]['max']+1?>);
+			}
+		});
+		function checkbox_xyft(){
+			ractive.xyft();
+		}
+		function checkbox_pk10(){
+			ractive.pk10();
+		}
 		function pushbtn(){
 			$.ajax({
 			url:'betadd.php',
